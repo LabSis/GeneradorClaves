9ecc9be6ee2fa6f7bef89494859f64feead546ab
Paula <paulabeatrizolmedo@gmail.com>
2017-08-17 20:51:36 -0300

    Agregadas validaciones

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,35 +5,49 @@
  */
 
 import Lógica.ControladorPrincipal;
+import java.util.regex.Pattern;
 /**
  *
  * @author paula
  */
 public class Main {
+    /*
+        Carga de datos o búsqueda de hash, según los argumentos recibidos. 
+        @param args[0]: <run> para realizar una búsqueda, <load> para cargar datos
+               args[1]: tipo de hash
+               args[2]: hash a crackear o longitud de las claves a generar.
+    */
     public static void main(String args[]) {
-   
-        //args[0] = acción a realizar
-        //<run> para hacer una búsqueda
-        //<load> para cargar datos
+
+       Pattern patronHash = Pattern.compile("^[a-zA-Z0-9]+$");
+       Pattern patronTipoHash = Pattern.compile("^[a-zA-Z]{3}[0-9]{1}$");
        try{
-            if (args[0].compareTo("run") == 0){
-                //búsqueda hashes
-                //args[1] = tipo de hash
-                //args[2] = hash a crackear
-                hashInverso(args);
+           //compruebo que la cantidad de argumentos ingresados sea válida
+           if (args.length==3) {
+               if (args[0].compareTo("run") == 0){
+                
+                   //verifico que el hash contenga caracteres válidos
+                   if (patronTipoHash.matcher(args[1]).find() && patronHash.matcher(args[2]).find()) {
+                    //verifico que el largo del hash ingresado se encuentre en un rango válido
+                    if (args[2].length()>=32 && args[2].length()<=128) {
+                        hashInverso(args);
+                    }
+                   }
+
             }
-            else{
-               if(args[0].compareTo("load") == 0){
-                //carga masiva
-                //args[1] = longitud clave
-                //args[2] = tipo de hash
-                guardarClaves(args);
-               }
                else{
-                   //error de argumentos
-               }
+                   if(args[0].compareTo("load") == 0){
+                       if (Integer.parseInt(args[2]) < 12) {
+                           guardarClaves(args);
+                       }
+                   }
             }}
-        catch(Exception e){
+          else{
+               System.out.println("Ingresar la cantidad correcta de argumentos.");
+           }
+           }
+            
+        catch(NumberFormatException e){
             System.out.println("Error de argumentos. ");
         }
         
@@ -42,7 +56,7 @@ public class Main {
     public static void guardarClaves(String args[]) {
         ControladorPrincipal controlador;
         //longitud clave, tipo de hash a generar
-        controlador = new ControladorPrincipal(Integer.parseInt(args[1]), args[2]);
+        controlador = new ControladorPrincipal(Integer.parseInt(args[2]), args[1]);
         //genera todas las claves con sus respectivos hash, según una longitud deseada y un tipo de hash determinado.
         controlador.guardarClavesArchivo();
     }
