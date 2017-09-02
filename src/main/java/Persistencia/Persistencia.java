6cfbfb6c2a27dcf82a68f41c4138c0e9290b67e8
Paula <paulabeatrizolmedo@gmail.com>
2017-09-02 13:44:52 -0300

    Validaciones

--- src/main/java/Persistencia/Persistencia.java
+++ src/main/java/Persistencia/Persistencia.java
@@ -29,46 +29,82 @@ public class Persistencia {
         @param tableName: nombre correspondiente a la tabla que se esté trabajando
     */
     public void initInsert(String keyspace, String tableName){
-        StringBuilder insert = new StringBuilder();
-        insert.append("INSERT INTO ");
-        insert.append(keyspace);
-        insert.append(".");
-        insert.append(tableName);
-        insert.append("(key, hash) VALUES (?,?)");
-           
-        statement = insert.toString();
+        try{
+            StringBuilder insert = new StringBuilder();
+            insert.append("INSERT INTO ");
+            insert.append(keyspace);
+            insert.append(".");
+            insert.append(tableName);
+            insert.append("(key, hash) VALUES (?,?)");
+            statement = insert.toString();
+        }
+        catch(Exception e){
+             System.out.println("Error de la base de datos: " + e.getLocalizedMessage());
+            //System.out.println("__ERROR__");
+            
+        }
+        
     }
     public void guardarClaves(String clave, String hash){
-       SESSION.execute(statement, clave, hash);
+       try{
+           SESSION.execute(statement, clave, hash);
+       }
+       catch(Exception e){
+           System.out.println("Error al guardar los datos: " + e.getLocalizedMessage());
+            //System.out.println("__ERROR__");
+       }
     }
     /*
         @param keyspace: nombre correspondiente al keyspace que se está trabajando... tener en cuenta que tengo
                          uno por cada hash trabajado. En este caso es necesario para saber en cuál me posiciono 
                          para hacer la búsqueda del hash
+                         (claves menores o iguales a 4)
     */
     public String initSelect(String keyspace){
-        StringBuilder select = new StringBuilder();
-        select.append("SELECT * FROM keys_");
-        select.append(keyspace);
-        select.append(".minor_keys");
-        select.append(" WHERE hash = ?");
-        select.append(" ALLOW FILTERING");
-                
-        statement = select.toString();
-        //statement = "SELECT * FROM pruebas.pruebas WHERE hash = ? ALLOW FILTERING";
-        return statement;
+        try{
+            StringBuilder select = new StringBuilder();
+            select.append("SELECT * FROM keys_");
+            select.append(keyspace);
+            select.append(".minor_keys");
+            select.append(" WHERE hash = ?");
+            select.append(" ALLOW FILTERING");
+
+            statement = select.toString();
+            //statement = "SELECT * FROM pruebas.pruebas WHERE hash = ? ALLOW FILTERING";
+            return statement;
+        }
+        catch(Exception e){
+            System.out.println("Error de la base de datos: " + e.getLocalizedMessage());
+            //System.out.println("__ERROR__");
+            return null;
+        }
+        
     }
     /*
-        trial
+    keyspace: según el algoritmo que esté utilizando
     */
-    public String initSelectBis(){
-        StringBuilder select = new StringBuilder();
-        select.append("SELECT * FROM pruebas.pruebas WHERE key = ?");
-        select.append("ALLOW FILTERING");
+    public String initSelectRT(String keyspace, int longitud){
         
-        statement = select.toString();
-        return statement;
+        try{
+            StringBuilder select = new StringBuilder();
+            select.append("SELECT * FROM keys_");
+            select.append(keyspace);
+            select.append(".keys_size_");
+            select.append(longitud);
+            select.append(" WHERE hash = ?");
+            select.append(" ALLOW FILTERING");
+
+            statement = select.toString();  
+            
+            return statement;
+        }
+        catch(Exception e){
+            System.out.println("Error de la base de datos: " + e.getLocalizedMessage());
+            //System.out.println("__ERROR__");
+            return null;
+        }
     }
+    
     /*
         @param hash: valor de hash al cual le quiero buscar una coincidencia
     */
@@ -77,51 +113,31 @@ public class Persistencia {
         try{
             ResultSet rs = SESSION.execute(statement, hash);
             Row row = rs.one();
-            if (row.getString("key") != null) {
+            if (!row.isNull("key")) {
                  key = row.getString("key");
-                 System.out.println("La clave es: " + key);
+                  //System.out.println("La clave es: " + key);
             }
         }
         catch(Exception e){
             //System.out.println("Error en la búsqueda: " + e.getLocalizedMessage());
             //System.out.println("__ERROR__");
         }
+       
         return key;
     }
-    public String yanose(String primeraCoincidencia){
+    /*
+        Dada una determinada palabra (hash reducido) busco si tiene una clave asociada
+    */
+    public boolean coincidenciaRT(String palabra){
         try{
-            ResultSet rs = SESSION.execute(statement, primeraCoincidencia);
+            ResultSet rs = SESSION.execute(statement, palabra);
             Row row = rs.one();
-            if (row.getString("hash") != null) {
-               return row.getString("hash");
-            }
+            if (!row.isNull("key")) {return true;}
         }
-        catch(Exception e){
-            System.out.println("...");
-        }
-        return null;
+        catch(Exception e){return false;}
+        return false;
     }
-    
-    public boolean existeCoincidencia(String palabra){
-        boolean flag = false;
-       
-        try{
-             ResultSet rs = SESSION.execute(statement, palabra);
-             Row row = rs.one();
-             if (row.getString("hash")!=null) {
-                flag = true;
-                
-             }
-        }
-        catch(Exception e){
-//            System.out.println("no hay coincidencias");
-        }
-        return flag;
-    }
-    
     public void cerrarConexion(){
         CONNECTOR.close();
-    }
-
-    
+    }   
 }
