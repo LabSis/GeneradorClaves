21782eb6162eab8b614fe3885052dc29025f1aba
Paula <paulabeatrizolmedo@gmail.com>
2017-09-02 14:20:25 -0300

    Aclaro topologías comunes

--- src/main/java/Lógica/Topologías.java
+++ src/main/java/Lógica/Topologías.java
@@ -13,38 +13,44 @@ import java.util.regex.Pattern;
  */
 public class Topologías {
     /*
-        Cadenas planas
+        1)
+        Sólo números
     */
-    private static final String TOPOLOGIA_1 = "";
+    private static final String TOPOLOGIA_1 = "^([0-9]+)$";
     /*
+        1) 
+        Sólo minúsculas
+        2) 
         Empieza con una o más minúsculas
         Seguido por 0 o más mayúsculas
         Seguido por 0 o más digitos numéricos
         
             l*d*s*      bb22..
-            l*d*u*      bb22BB #revisar qué tan frecuente es. 
             l*d*l*d*    bb22bb22
             l*s*        bb..
             l*s*d       bb..22
             l*s*l*s*    bb..bb..
-            l*u*d*      bbBB22 //la detecta
+            l*u*d*      bbBB22 
             l*u*l*u     bbBBbbBB
         
     */
-    private static final String TOPOLOGIA_2 = "^[a-z]+[A-Z]*[0-9]*$";
-    /*
+    private static final String TOPOLOGIA_2 = "^[a-z]+[A-Z]*[0-9]*";
+     /*
+        1) 
+        Sólo mayúsculas
+        2) 
         Empieza con una o más mayúsculas
         Seguido por 0 o más minúsculas
         Seguido por 0 o más digitos numéricos
-           
-            u*d*        BB22 //la detecta
+        Ejemplos: 
+            u*d*        BB22 /
             u*s*        BB..
             u*l*u*l*    BBbbBBbb
-            u*l*d*      BBbb22 //la detecta
+            u*l*d*      BBbb22 
             u*l*s*      BBbb..
             u*l*s*d*    BBbb..22
     */
-    private static final String TOPOLOGIA_3 = "^[A-Z]+[a-z]*[0-9]*$";
+    private static final String TOPOLOGIA_3 = "^[A-Z]+[a-z]*[0-9]*";
     /*
         Tipo 4: #ESTA LA IMPLEMENTO MÁS TARDE PORQUE NO ES TAN COMÚN
         d*d*l*      22bb
@@ -57,16 +63,14 @@ public class Topologías {
     */
     private static final String TOPOLOGIA_4 = "";
     
-    private static final String PRUEBA = "^([0-9]+)$";
-    
+      
     public boolean esTopologiaComun(String contraseña){
        
         Pattern p1 = Pattern.compile(TOPOLOGIA_1);
         Pattern p2 = Pattern.compile(TOPOLOGIA_2);
         Pattern p3 = Pattern.compile(TOPOLOGIA_3);
-        Pattern p4 = Pattern.compile(TOPOLOGIA_4);
 
         return p1.matcher(contraseña).find() || p2.matcher(contraseña).find() || p3.matcher(contraseña).find();
-   
+       
     }
 }
